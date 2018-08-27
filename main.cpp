1c2627192cc87716502db915736af94e16e8c8a4
SH <sh.ylzhxx@163.com>
2018-08-27 12:00:48 +0800

    refactor

--- main.cpp
+++ main.cpp
@@ -3,12 +3,6 @@
 #include <string.h>
 #include <arpa/inet.h>
 
-struct epoll_data
-{
-	int fd;
-	int status;
-	long id;
-};
 
 int main(int argc,char *argv[]){
 	daemonize("push_server");
@@ -26,9 +20,9 @@ int main(int argc,char *argv[]){
 	char ack_ok[7]={0,0,0,7,'2','0','0'};
 
 	struct epoll_event ev, events[MAX_EVENTS];
-	int servfd, sockfd, n, nfds, epollfd;
-	unsigned int len,len_recv,num;
-	unsigned long id;
+	uint32_t servfd, sockfd, n, nfds, epollfd;
+	uint32_t len,len_recv,num;
+	uint64_t id;
 
 	if((servfd=initTcpServer(SERVER_PORT))==-1){
 		logger.printf("initTcpServer failed\n");
@@ -42,7 +36,7 @@ int main(int argc,char *argv[]){
 	}
 
 	ev.events = EPOLLIN;
-	ev.data.fd = servfd;
+	ev.data.u64=servfd;
 	if (epoll_ctl(epollfd, EPOLL_CTL_ADD, servfd, &ev) == -1) {
 		logger.printf("epoll_ctl: servfd failed\n");
 		exit(-1);
@@ -58,7 +52,8 @@ int main(int argc,char *argv[]){
 	   logger.printf("nfds %d\n",nfds);
 
 	   for (n = 0; n < nfds; ++n) {
-	       if (events[n].data.fd == servfd) {
+	   		sockfd=events[n].data.u64;
+	       if (sockfd == servfd) {
 	           sockfd = accept(servfd,NULL,NULL);
 	           logger.printf("accept %d\n",sockfd);
 	           if (sockfd == -1) {
@@ -66,14 +61,14 @@ int main(int argc,char *argv[]){
 	        		exit(-1);
 	           }
 	           ev.events = EPOLLIN | EPOLLET;
-	           ev.data.fd = sockfd;
+	           ev.data.u64 = sockfd;
 	           if (epoll_ctl(epollfd, EPOLL_CTL_ADD, sockfd,&ev) == -1) {
 					logger.printf("epoll_ctl: sockfd failed\n");
 					exit(-1);
 	           }
 	       } else {
 	       		//handle data
-	       		sockfd=events[n].data.fd;
+	       		id=events[n].data.u64>>32;
 	       		logger.printf("handle data %d\n",sockfd);
 	       		if(recv(sockfd,&len,4,MSG_DONTWAIT|MSG_PEEK)!=4){
 	       			logger.printf("wait more data\n");
@@ -82,8 +77,8 @@ int main(int argc,char *argv[]){
 	       		len=ntohl(len);
 	       		if(len>MAX_MESSAGE_SIZE || len<5 || ioctl(sockfd,FIONREAD,&len_recv)==-1){
 	       			logger.printf("invalid len or ioctl failed\n");
-	       			if(events[n].data.u32==1){
-	       				data.remove(events[n].data.u64);
+	       			if(id!=0){
+	       				data.remove(id);
 	       			}else{
 	       				close(sockfd);
 	       			}
@@ -95,8 +90,8 @@ int main(int argc,char *argv[]){
 	       		}
 	       		if(recv(sockfd,buff,len,MSG_DONTWAIT)!=len){
 	       			logger.printf("recv len != len\n");
-	       			if(events[n].data.u32==1){
-	       				data.remove(events[n].data.u64);
+	       			if(id!=0){
+	       				data.remove(id);
 	       			}else{
 	       				close(sockfd);
 	       			}
@@ -105,24 +100,23 @@ int main(int argc,char *argv[]){
 
 	       		//process data
 	       		if(buff[4]==1){//bind
-	       			if(events[n].data.u32==1){//already bound
-	       				data.remove(events[n].data.u64);
+	       			if(id!=0){//already bound
+	       				data.remove(id);
 	       				continue;
 	       			}
-	       			if(len!=13){//invalid param
+	       			if(len!=9){//invalid param
 	       				logger.printf("invalid param\n");
 	       				close(sockfd);
 	       				continue;
 	       			}
-	       			id=ntohl64(buff+5);
+	       			memcpy(&id,buff+5,4);
+	       			id=ntohl(id);
 	       			if(send(sockfd,ack_ok,7,MSG_NOSIGNAL)==-1){
 	       				logger.printf("send ack failed\n");
 	       				close(sockfd);
 	       			}else{//bind success
 	       				ev.events = EPOLLIN | EPOLLET;
-						ev.data.fd = sockfd;
-						ev.data.u32=1;//标记为已绑定
-						ev.data.u64=id;
+						ev.data.u64=id<<32 | sockfd;
 	       				if (epoll_ctl(epollfd, EPOLL_CTL_MOD, sockfd,&ev) == -1) {
 							logger.printf("epoll_ctl: EPOLL_CTL_MOD failed\n");
 							close(sockfd);
@@ -133,8 +127,8 @@ int main(int argc,char *argv[]){
 	       		}else if(buff[4]==2){//push
 	       			if(len<9){
 	       				logger.printf("cmd 2 :len(%d)<9\n",len);
-	       				if(events[n].data.u32==1){
-		       				data.remove(events[n].data.u64);
+	       				if(id!=0){
+		       				data.remove(id);
 		       			}else{
 		       				close(sockfd);
 		       			}
@@ -142,20 +136,21 @@ int main(int argc,char *argv[]){
 	       			}
 	       			memcpy(&num,buff+5,4);
 	       			num=ntohl(num);
-	       			if(len<(9+num*8)){
-	       				logger.printf("cmd 2 :len(%d) <%d\n", len,9+num*8);
-	       				if(events[n].data.u32==1){
-		       				data.remove(events[n].data.u64);
+	       			if(len<(9+num*4)){
+	       				logger.printf("cmd 2 :len(%d) <%d\n", len,9+num*4);
+	       				if(id!=0){
+		       				data.remove(id);
 		       			}else{
 		       				close(sockfd);
 		       			}
 	       				continue;
 	       			}
-	       			const char *content=buff+9+num*8;
-	       			len-=9+num*8;
+	       			const char *content=buff+9+num*4;
+	       			len-=9+num*4;
 	       			int len_send=htonl(len+4);
 	       			for(int i=0;i<num;++i){
-	       				id=ntohl64(buff+9+8*i);
+	       				memcpy(&id,buff+9+4*i,4);
+	       				id=ntohl(id);
 	       				if(p=(client *)data.search(id,false)){
 	       					if(send(p->fd,&len_send,4,MSG_NOSIGNAL)==-1 || send(p->fd,content,len,MSG_NOSIGNAL)==-1){
 	       						data.remove(id);
@@ -164,8 +159,8 @@ int main(int argc,char *argv[]){
 	       				}
 	       			}
 	       		}else{//unknown cmd
-	       			if(events[n].data.u32==1){
-	       				data.remove(events[n].data.u64);
+	       			if(id!=0){
+	       				data.remove(id);
 	       			}else{
 	       				close(sockfd);
 	       			}
--- push.h
+++ push.h
@@ -99,5 +99,3 @@ int initTcpServer(const char * port);
 int initTcpClient(const char *ip,const char *port);
 
 int daemonize(const char *cmd);
-
-unsigned long ntohl64(char *buff);
