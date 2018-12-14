d3458c8816c74ca4c711d02454f5de9f6a57e071
SH <sh.ylzhxx@163.com>
2018-12-14 18:55:07 +0800

    a

--- main.cpp
+++ main.cpp
@@ -206,6 +206,7 @@ int main(int argc,char *argv[]){
 	       						data.erase(id);
 	       						close(sockfd);
 	       						logger.printf("(id:%ld) push failed,broken link\n",id);
+	       						logger.printf("errno:%d\n", errno);
 	       					}
 	       				}
 	       			}
